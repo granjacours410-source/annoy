44a9477cfa896640f0f79c281c626d679895288d
Michael Pilosov <consistentbayes@gmail.com>
2025-06-20 01:22:25 +0000

    build sdist

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ if manual_linker_args:
     extra_link_args = manual_linker_args.split(',')
 
 setup(name='annoy-mm',
-      version='1.17.4rc0',
+      version='1.17.4rc1',
       description='Approximate Nearest Neighbors in C++/Python optimized for memory usage and loading/saving to disk.',
       packages=['annoy'],
       package_data={'annoy': ['__init__.pyi', 'py.typed']},
